825e5468281045984749a8f6116cee42666e3636
hachard <hubert.achard@gmail.com>
2016-12-20 10:58:14 -0500

    saving result in array

--- app/crawl.py
+++ app/crawl.py
@@ -2,14 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-class crawler(serie):
+
+a = []
+
+class crawler():
     URL = "https://eztv.ag"
     name = "elementary"
     r = requests.get(URL + "/search/" + name, timeout=5)
     data = r.text
     soup = BeautifulSoup(data, 'html.parser')
     for link in soup.find_all('a', {'class': 'epinfo'}):
-        toto = (link.get('title'))
-        result = re.search(r".*720.*", toto)
-        if result:
-            print (result.group(0))
+        result = (link.get('title'))
+        a.append(result)
+        print (a)
