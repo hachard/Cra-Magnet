228e8f3f498a8a43cf7928f17b9a4c470a8acde3
hachard <hubert.achard@gmail.com>
2016-12-20 17:06:31 -0500

    working html

--- app/crawl.py
+++ app/crawl.py
@@ -2,16 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-
-a = []
-
 class crawler():
     URL = "https://eztv.ag"
     name = "elementary"
+    a = []
     r = requests.get(URL + "/search/" + name, timeout=5)
     data = r.text
     soup = BeautifulSoup(data, 'html.parser')
     for link in soup.find_all('a', {'class': 'epinfo'}):
         result = (link.get('title'))
         a.append(result)
-        print (a)
--- app/views.py
+++ app/views.py
@@ -1,21 +1,13 @@
 from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
+from .crawl import crawler
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'toto'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'Joe'},
-            'body': 'Lorem ipsum dolor sit amet.'
-        },
-        {
-            'author': {'nickname': 'Sus'},
-            'body': 'Excepteur sint occaecat cupidatat non proident!'
-        }
-    ]
+    posts = crawler.a
     return render_template("index.html",
                            title='Home',
                            user=user,
