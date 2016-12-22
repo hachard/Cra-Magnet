85d7753ceaa81bab5975d700f8fde74d952c1a23
hachard <hubert.achard@gmail.com>
2016-12-21 21:40:34 -0500

    Very alpha

--- app/crawl.py
+++ app/crawl.py
@@ -1,14 +1,26 @@
 from bs4 import BeautifulSoup
+from flask import request
 import requests
 import re
+import pprint
 
-class crawler():
-    URL = "https://eztv.ag"
-    name = "elementary"
-    a = []
-    r = requests.get(URL + "/search/" + name, timeout=5)
-    data = r.text
-    soup = BeautifulSoup(data, 'html.parser')
-    for link in soup.find_all('a', {'class': 'epinfo'}):
-        result = (link.get('title'))
-        a.append(result)
+class crawler(object):
+    def __init__(self, a):
+        self.a = a
+
+    def tv_show():
+        URL = "https://eztv.ag"
+        name = request.form.get('text', "default_value")
+        a = {}
+        r = requests.get(URL + "/search/" + name, timeout=5)
+        data = r.text
+        soup = BeautifulSoup(data, 'html.parser')
+        for link in soup.find_all('a', {'class': 'magnet'}, limit=5):
+            result = (link.get('title'))
+            result = result.replace(' Magnet Link', '')
+            result = result.replace(' Torrent:', '')
+            magnet = (link.get('href'))
+            magnet = magnet.replace('&amp;', '&')
+            dic = {result:magnet}
+            a.update(dic)
+        return a
--- app/views.py
+++ app/views.py
@@ -1,18 +1,14 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
+import requests
+import re
 from .crawl import crawler
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def my_form_post():
     user = {'nickname': 'toto'}  # fake user
-    posts = crawler.a
+    posts = crawler.tv_show()
     return render_template("index.html",
                            title='Home',
                            user=user,
                            posts=posts)
-@app.route('/list')
-def list():
-    serie = crawler()
-    return render_template("list.html")
